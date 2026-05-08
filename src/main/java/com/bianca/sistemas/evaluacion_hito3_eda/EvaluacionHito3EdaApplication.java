39c4ae52703ab2b0ca13071bdec08a2e3aeefe87
BIANCA8080 <riveramoira2007@gmail.com>
2026-05-08 11:59:49 -0600

    Estructura inicial del proyecto - Bianca Rivera

---
+++ src/main/java/com/bianca/sistemas/evaluacion_hito3_eda/EvaluacionHito3EdaApplication.java
@@ -0,0 +1,13 @@
+package com.bianca.sistemas.evaluacion_hito3_eda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionHito3EdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionHito3EdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bianca/sistemas/evaluacion_hito3_eda/EvaluacionHito3EdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bianca.sistemas.evaluacion_hito3_eda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionHito3EdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
